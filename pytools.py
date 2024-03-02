f51c736ca46c12606bf4dd9ca75ec9b6d1bea51b
David Lewallen <davidilewallen@gmail.com>
2024-03-01 22:42:26 -0800

    added couple pages

--- pytools.py
+++ pytools.py
@@ -11,3 +11,8 @@ def index():
 @app.route('/about')
 def about():
 	return render_template('about.html')
+
+# Create User Page
+@app.route('/user/<name>')
+def user(name):
+	return render_template('user.html', user_name=name)
